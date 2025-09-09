300930822185cbdbd4d2dffcec6c86dfe746b35e
murojun <d21137@numazu.kosen-ac.jp>
2025-09-09 15:41:28 +0900

    バグ修正

--- mirs24_esp32/imu.ino
+++ mirs24_esp32/imu.ino
@@ -7,17 +7,17 @@ void imu_setup(){
 
   // 外部クリスタルを使用する設定（より精度が安定します）
   bno.setExtCrystalUse(true);
-  vlt_msg.data.size = 9; // メッセージ配列のサイズを2に設定
-  vlt_msg.data.data = (double *)malloc(enc_msg.data.size * sizeof(double)); // 配列のメモリを確保
-  vlt_msg.data.data[0] = 0;
-  vlt_msg.data.data[1] = 0;
-  vlt_msg.data.data[2] = 0;
-  vlt_msg.data.data[3] = 0;
-  vlt_msg.data.data[4] = 0;
-  vlt_msg.data.data[5] = 0;
-  vlt_msg.data.data[6] = 0;
-  vlt_msg.data.data[7] = 0;
-  vlt_msg.data.data[8] = 0;
+  imu_msg.data.size = 9; // メッセージ配列のサイズを2に設定
+  imu_msg.data.data = (double *)malloc(imu_msg.data.size * sizeof(double)); // 配列のメモリを確保
+  imu_msg.data.data[0] = 0;
+  imu_msg.data.data[1] = 0;
+  imu_msg.data.data[2] = 0;
+  imu_msg.data.data[3] = 0;
+  imu_msg.data.data[4] = 0;
+  imu_msg.data.data[5] = 0;
+  imu_msg.data.data[6] = 0;
+  imu_msg.data.data[7] = 0;
+  imu_msg.data.data[8] = 0;
   delay(1000);
 }
   
